4700970777c9f0ecb542dc23f7ff57a55235e1f8
Brian Steffens <briansteffens@gmail.com>
2016-11-15 22:38:52 -0500

    Remove element pre-render stuff

--- main.c
+++ main.c
@@ -1,32 +1,6 @@
-#include <stdbool.h>
 #include <stdlib.h>
 
 #include <ncurses.h>
-
-#define UNDEF -1
-
-typedef enum
-{
-    SOURCE,
-    LAYOUT
-} managed_by;
-
-typedef struct
-{
-    unsigned char render;
-    bool active;
-    int source_byte;
-    int source_nibble;
-    managed_by managed_by;
-} element;
-
-typedef struct
-{
-    int left;
-    int top;
-    int width;
-    int height;
-} rect;
 
 typedef struct
 {
@@ -37,23 +11,14 @@ typedef struct
     int height;
 } pane;
 
-void destroy_pane(pane* pane)
-{
-    //wborder(pane->window, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
-    //wrefresh(pane->window);
-    delwin(pane->window);
-}
-
 void setup_pane(pane* pane)
 {
     if (pane->window)
     {
-        destroy_pane(pane);
+        delwin(pane->window);
     }
 
     pane->window = newwin(pane->height, pane->width, pane->top, pane->left);
-    //box(pane->window, 0, 0);
-    //wrefresh(pane->window);
 }
 
 #define SOURCE_LEN 256
@@ -63,9 +28,6 @@ void setup_pane(pane* pane)
 typedef struct
 {
     unsigned char source[SOURCE_LEN];
-
-    element elements[MAX_OUTPUT_WIDTH * MAX_OUTPUT_HEIGHT];
-    int elements_len;
 
     int cursor_byte;
     int cursor_nibble;
@@ -97,20 +59,6 @@ void byte_to_hex(unsigned char byte, char* hex)
     hex[1] = nibble_to_hex(second);
 }
 
-int source_to_element(state* state, int source_byte, int source_nibble)
-{
-    for (int i = 0; i < state->elements_len; i++)
-    {
-        if (state->elements[i].source_byte == source_byte &&
-            state->elements[i].source_nibble == source_nibble)
-        {
-            return i;
-        }
-    }
-
-    exit(1);
-}
-
 void render_details(state* state, pane* pane)
 {
     wclear(pane->window);
@@ -140,6 +88,11 @@ int byte_in_line(state* state, int byte_offset)
     return byte_offset / bytes_per_line(state);
 }
 
+int byte_in_column(state* state, int byte_offset)
+{
+    return byte_offset % bytes_per_line(state) * (state->grouping * 2 + 1);
+}
+
 int first_byte_in_line(state* state, int line_index)
 {
     return line_index * bytes_per_line(state);
@@ -162,7 +115,9 @@ int last_visible_line(state* state)
 
 int last_visible_byte(state* state)
 {
-    return last_byte_in_line(state, last_visible_line(state));
+    int ret = last_byte_in_line(state, last_visible_line(state));
+
+    return ret < SOURCE_LEN ? ret : SOURCE_LEN - 1;
 }
 
 int main()
@@ -178,11 +133,6 @@ int main()
         state.source[i] = (unsigned char)i;
     }
 
-    for (int i = 0; i < MAX_OUTPUT_WIDTH * MAX_OUTPUT_HEIGHT; i++)
-    {
-        state.elements[i].active = false;
-    }
-
     initscr();
     cbreak();
     keypad(stdscr, TRUE);
@@ -195,14 +145,12 @@ int main()
     pane detail_pane;
     detail_pane.left = 10;
 
-    int last_max_x;
-    int last_max_y;
+    int last_max_x = -1;
+    int last_max_y = -1;
     int max_x;
     int max_y;
 
     getmaxyx(stdscr, max_y, max_x);
-    last_max_x = -1;
-    last_max_y = -1;
 
     int ch;
     while ((ch = getch()) != KEY_F(1))
@@ -232,7 +180,6 @@ int main()
             setup_pane(&detail_pane);
         }
 
-        int element_index;
         int temp;
 
         switch (ch)
@@ -288,6 +235,20 @@ int main()
                 break;
         }
 
+        // Clamp to start of buffer
+        if (state.cursor_byte < 0)
+        {
+            state.cursor_byte = 0;
+            state.cursor_nibble = 0;
+        }
+
+        // Clamp to end of buffer
+        if (state.cursor_byte >= SOURCE_LEN)
+        {
+            state.cursor_byte = SOURCE_LEN - 1;
+            state.cursor_nibble = 1;
+        }
+
         // Scroll up if cursor has left viewport
         while (state.cursor_byte < first_visible_byte(&state))
         {
@@ -306,142 +267,34 @@ int main()
             state.scroll_start = 0;
         }
 
-        // Clamp to start of buffer
-        if (state.cursor_byte < 0)
-        {
-            state.cursor_byte = 0;
-            state.cursor_nibble = 0;
-        }
-
-        // Clamp to end of buffer
-        if (state.cursor_byte >= SOURCE_LEN)
-        {
-            state.cursor_byte = SOURCE_LEN - 1;
-            state.cursor_nibble = 1;
-        }
-
-
-        int render_cursor_x = 0;
-        int render_cursor_y = 0;
-
-        for (int i = 0; i < MAX_OUTPUT_WIDTH * MAX_OUTPUT_HEIGHT; i++)
-        {
-            state.elements[i].active = false;
-        }
-
-        // Render hex data to state.elements
-        int out_x = 0;
-        int out_y = 0;
-        char hex[2];
-        int element_i = 0;
-        state.elements_len = 0;
-        int si = first_visible_byte(&state);
-        int last_byte = last_visible_byte(&state);
-        if (last_byte >= SOURCE_LEN)
-        {
-            last_byte = SOURCE_LEN - 1;
-        }
-        int di = 0;
-        while (si <= last_byte)
-        {
-            unsigned char byte = state.source[si];
-            byte_to_hex(byte, hex);
-
-            if (hex_pane.width - out_x < 2)
-            {
-                // Newline
-                while (out_x < hex_pane.width)
-                {
-                    element_i = out_y * hex_pane.width + out_x;
-                    state.elements[element_i].active = false;
-                    state.elements_len++;
-                    out_x++;
-                }
-
-                out_x = 0;
-                out_y++;
-            }
-
-            if (out_y > hex_pane.height - 1)
-            {
-                // Out of bounds
-                break;
-            }
-
-            if (state.cursor_byte == si)
-            {
-                render_cursor_x = hex_pane.left + out_x;
-                render_cursor_y = hex_pane.top + out_y;
-
-                if (state.cursor_nibble == 1)
-                {
-                    render_cursor_x++;
-                }
-            }
-
-            element_i = out_y * hex_pane.width + out_x;
-            state.elements[element_i].render = hex[0];
-            state.elements[element_i].active = true;
-            state.elements[element_i].source_byte = si;
-            state.elements[element_i].source_nibble = 0;
-            state.elements[element_i].managed_by = SOURCE;
-            state.elements_len++;
-            out_x++;
-
-            element_i = out_y * hex_pane.width + out_x;
-            state.elements[element_i].render = hex[1];
-            state.elements[element_i].active = true;
-            state.elements[element_i].source_byte = si;
-            state.elements[element_i].source_nibble = 1;
-            state.elements[element_i].managed_by = SOURCE;
-            state.elements_len++;
-            out_x++;
-
-            if (out_x < hex_pane.width - 1)
-            {
-                // Room for a space
-                element_i = out_y * hex_pane.width + out_x;
-                state.elements[element_i].render = ' ';
-                state.elements[element_i].active = true;
-                state.elements[element_i].source_byte = UNDEF;
-                state.elements[element_i].source_nibble = UNDEF;
-                state.elements[element_i].managed_by = LAYOUT;
-                state.elements_len++;
-                out_x++;
-            }
-
-            si++;
-            di++;
-        }
-
+        // Render hex data
         wclear(hex_pane.window);
-        for (int y = 0; y < hex_pane.height; y++)
+        char hex[2];
+        for (int i = first_visible_byte(&state);
+             i <= last_visible_byte(&state); i++)
         {
-            for (int x = 0; x < hex_pane.width; x++)
-            {
-                int src = y * hex_pane.width + x;
+            byte_to_hex(state.source[i], hex);
 
-                if (!state.elements[src].active)
-                {
-                    continue;
-                }
+            int out_y = byte_in_line(&state, i) - state.scroll_start;
+            int out_x = byte_in_column(&state, i);
 
-                mvwaddch(hex_pane.window, y, x, state.elements[src].render);
-            }
+            mvwprintw(hex_pane.window, out_y, out_x, "%c%c ", hex[0], hex[1]);
         }
-        //wrefresh(hex_pane.window);
 
         render_details(&state, &detail_pane);
 
-        move(render_cursor_y, render_cursor_x);
-        //wmove(hex_pane.window, 0, 0);
+        // Place cursor
+        int render_cursor_x = hex_pane.left +
+            byte_in_column(&state, state.cursor_byte) + state.cursor_nibble;
+        int render_cursor_y = hex_pane.top +
+            byte_in_line(&state, state.cursor_byte) - state.scroll_start;
 
+        move(render_cursor_y, render_cursor_x);
+
+        // Flush output
         wnoutrefresh(hex_pane.window);
         wnoutrefresh(detail_pane.window);
 
         doupdate();
-
-        //refresh();
-        //doupdate();
     }
 }
